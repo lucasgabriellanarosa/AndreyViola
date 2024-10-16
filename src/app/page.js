2a85da793638b0ef5c5edd6a52f82de069bfbfbe
Lucas Gabriel de Lana Rosa <lucas.gabriel.lr2112@gmail.com>
2024-10-16 19:11:44 -0300

    hero section + biografy

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,48 @@
+import { FaYoutube, FaInstagram, FaWhatsapp } from "react-icons/fa";
 import Image from "next/image";
-import styles from "./page.module.css";
+import Andrey from "./assets/andrey.jpg"
+import Biografia from "./assets/biografia.jpg"
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="bg-gray-300 min-h-screen pb-4">
+      <header className="bg-gradient-to-r from-black to-blue-950 shadow-2xl flex py-4 justify-center">
+        <h1 className="text-white text-5xl font-italianno">Andrey Viola</h1>
+        <ul className="hidden">
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+          <li className="text-white">
+            <FaInstagram />
+          </li>
+
+          <li className="text-white">
+            <FaWhatsapp />
+          </li>
+
+          <li className="text-white">
+            <FaYoutube />
+          </li>
+
+        </ul>
+      </header>
+      <main className="flex flex-col gap-6 justify-center items-center">
+        <Image
+          src={Andrey}
+          alt="andrey viola"
+          className="shadow-2xl"
+        />
+        <div className="w-3/4 h-96 overflow-scroll bg-[url('assets/biografia.jpg')] bg-cover">
+          <div className="bg-black bg-opacity-60 w-full p-5 flex flex-col gap-2">
+            <h1 className="font-italianno text-white text-6xl">Biografia</h1>
+            <p className="text-base text-gray-300">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac urna tristique, tempor justo quis, malesuada odio. Nunc rutrum augue mi, sed feugiat lorem sagittis vitae. Nulla sed erat accumsan, suscipit metus ac, mattis sapien. Ut volutpat rutrum sagittis. Praesent ultricies neque eu iaculis suscipit. Vivamus mattis, nisl ut egestas molestie, leo justo iaculis sem, ac scelerisque elit ante viverra justo. Morbi quam nulla, accumsan at faucibus quis, egestas vel eros. Ut ut vestibulum lectus, quis aliquam nisi. Fusce eu mi a felis semper egestas at eu sem.
+
+              Praesent pulvinar sem et tempor tincidunt. Nulla tincidunt, diam non sollicitudin volutpat, purus nibh dignissim elit, non elementum urna sapien vel felis. Proin dui est, cursus eget dictum non, blandit eget tortor. Quisque eu ante at orci aliquam condimentum. Pellentesque iaculis laoreet nisl sed blandit. Sed quis ornare nulla, eget cursus purus. Aliquam et feugiat est. Nam consequat tellus quis aliquam convallis. Nam vel facilisis dui. In hac habitasse platea dictumst. Vestibulum feugiat lorem lectus, non euismod neque tempor ut. Vivamus condimentum turpis purus, vitae elementum nulla tempus vitae. Suspendisse suscipit turpis ac nisl consectetur, at consectetur purus volutpat.
+            </p>
+          </div>
+
         </div>
+
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
